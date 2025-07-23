e109979b19374d900739a7b500916d8dfe66e913
Daniel <dani.07rojas94@gmail.com>
2025-07-23 08:46:51 -0500

    Despliegue azure v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'      
-import { store } from './store'
+import { store } from './store/Index.js'
 
 import { TamaguiProvider } from 'tamagui'
 import { config as tamaguiConfig } from '../tamagui.config'
